3fcee34dce99517ebfb3309e5058678354e4b7f6
luka-danger <lukathevegan@gmail.com>
2024-02-10 15:53:34 -0700

    Write JS function to check if passwords match

---
+++ sign-up/script.js
@@ -0,0 +1,6 @@
+function onChange() {
+    let password = document.querySelector('input[name=password');
+    let confirm = document.querySelector('input[name=confirm');
+    (confirm.value === password.value)
+    ? confirm.setCustomValidity('') : confirm.setCustomValidity('Password does not match.');
+}
