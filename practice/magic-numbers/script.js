af590b3857b502e2cb73a59953f3b6a20b66f77c
luka-danger <lukathevegan@gmail.com>
2024-02-18 19:55:34 -0700

    Create font change function

--- practice/magic-numbers/script.js
+++ practice/magic-numbers/script.js
@@ -2,3 +2,20 @@ function reverseMe(e) {
     const reverse = document.querySelector('.container');
     reverse.classList.toggle('reverse');
 }
+
+function changeColor(e) {
+    const color = document.querySelectorAll('.numbers');
+    color.forEach(color => {
+        const changeColor = randomColor();
+        color.style.color = changeColor;
+    });
+}
+
+function randomColor() {
+    let randomRed = Math.floor(Math.random() * 256);
+    let randomGreen = Math.floor(Math.random() * 256);
+    let randomBlue = Math.floor(Math.random() * 256);
+    return `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+}
+
+
