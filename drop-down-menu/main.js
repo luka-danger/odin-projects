9706030747f96b7fdfb21ca5b6148e5dff4bbf16
luka-danger <lukathevegan@gmail.com>
2024-12-11 17:59:44 -0700

    Add image carousel

--- drop-down-menu/main.js
+++
@@ -1,9 +0,0 @@
-import { clickDropDown } from "./click-drop.js"
-import { hoverDropDown } from "./hover-drop.js"
-import { hideMenuOnStart } from "./hide-menu.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    hideMenuOnStart();
-    clickDropDown();
-    hoverDropDown();
-})
--- practice/CSS-Concepts/script.js
+++
@@ -1,4 +0,0 @@
-const showDialogBtn = document.getElementById('showDialogBtn');
-const favDialog = document.getElementById('favDialog');
-
-showDialogBtn.addEventListener('click', () => favDialog.showModal());
