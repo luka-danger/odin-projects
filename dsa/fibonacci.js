b261d6e010c41b05a4794ab92d76f3c21f7c4e95
luka-danger <lukathevegan@gmail.com>
2025-02-03 21:11:36 -0700

    Add fibonacci functions and console output

--- dsa/fibonacci.js
+++ dsa/fibonacci.js
@@ -30,7 +30,7 @@ function fibonacciWhileLoop(length) {
         y = z
         i++
     }
-    return `While Loop: [${myArr}]`
+    return myArr
 }
 
 function fibonacciForLoop(length) {
@@ -52,12 +52,35 @@ function fibonacciForLoop(length) {
         x = y
         y = z
     }
-    return `For Loop: [${myArr}]`
+    return myArr
 }
 
+function fibonacciRecursion(length) {
+    // Return empty array for 0 or negative values
+    if (length <= 0) return []
+    // Base case
+    if (length === 1) return [0]
+    if (length === 2) return [0, 1]
 
+    const fibonacci = fibonacciRecursion(length - 1);
 
+    // Add sum of last 2 elements
+    fibonacci.push(fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2]);
 
+    return fibonacci
+}
+
+// Output to Console
+console.log('--------Test Loop-----------')
+testLoop()
+console.log("")
+console.log('--------Fibonacci-----------')
+console.log(`While Loop:  [${fibonacciWhileLoop(10)}]`)
+console.log(`For Loop: [${fibonacciForLoop(10)}]`)
+console.log(`Recursion [${fibonacciRecursion(10)}]`)
+console.log("")
+
+/* USE TO CALL FUNCTIONS VIA COMMAND LINE :) 
 
 // Read command-line argument 
 const args = process.argv.slice(2)  // The first two elements are node and the script path, so slice to get actual arguments
@@ -67,8 +90,10 @@ const length = parseInt(args[0], 10)  // Convert the argument to an integer
 if (!isNaN(length)) {
     testLoop()
     // console.log(fibonacciWhileLoop(length));
-    console.log(fibonacciForLoop(length))
+    // console.log(fibonacciForLoop(length))
+    // console.log(fibonacciRecursion(length))
 } else {
     console.log("Please provide a valid number.")
 }
- 
+
+*/ 
