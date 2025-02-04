925e79deda16df6c6b3e7567f547dd8b89f49aa0
luka-danger <lukathevegan@gmail.com>
2025-02-03 19:53:31 -0700

    Create fibonacci and merge sort JS files

---
+++ dsa/fibonacci.js
@@ -0,0 +1,20 @@
+// test Node CLI
+function thisLoop() {
+    let arr = ['nate', 'waffles', 'paulina']
+
+    for (element in arr) {
+        // Output each element of array
+        console.log(arr[element])
+    }
+}
+
+function fibsLength(length) {
+    myArr = []
+    if (length == 0) {
+        m
+    }
+}
+
+function fibonacci(array) {
+
+}
