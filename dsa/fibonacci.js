70b56b2caa1572cbd55669a6c9946d4c6887d437
luka-danger <lukathevegan@gmail.com>
2025-02-03 20:44:00 -0700

    Create for loop

--- dsa/fibonacci.js
+++ dsa/fibonacci.js
@@ -33,14 +33,42 @@ function fibonacciWhileLoop(length) {
     return `While Loop: [${myArr}]`
 }
 
-// Read command-line argument
-const args = process.argv.slice(2);  // The first two elements are node and the script path, so slice to get actual arguments
-const length = parseInt(args[0], 10);  // Convert the argument to an integer
+function fibonacciForLoop(length) {
+    // Return empty array for 0 or negative values
+    if (length <= 0) return []
+    // Base case
+    if (length === 1) return [0]
+
+    // Initialize array w/ first 2 values of Fibonacci sequence
+    myArr = [0, 1]
+    let x = 0
+    let y = 1
+    let z
+    
+    // Start from third element since 2 items in array
+    for (let i = 2; i < length; i++) {
+        z = x + y
+        myArr.push(z)
+        x = y
+        y = z
+    }
+    return `For Loop: [${myArr}]`
+}
+
+
+
+
+
+// Read command-line argument 
+const args = process.argv.slice(2)  // The first two elements are node and the script path, so slice to get actual arguments
+const length = parseInt(args[0], 10)  // Convert the argument to an integer
 
 // Call the function and output the result
 if (!isNaN(length)) {
     testLoop()
-    console.log(fibonacciWhileLoop(length));
+    // console.log(fibonacciWhileLoop(length));
+    console.log(fibonacciForLoop(length))
 } else {
-    console.log("Please provide a valid number.");
+    console.log("Please provide a valid number.")
 }
+ 
