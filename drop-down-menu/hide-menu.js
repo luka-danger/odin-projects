68888b781f2b9e135f99f424f66a4ce09d52ea8b
luka-danger <lukathevegan@gmail.com>
2025-02-05 21:18:53 -0700

    Recursive sort left and right

--- drop-down-menu/hide-menu.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById('menu')
-const hoverMenu = document.getElementById('hover-menu')
-
-export function hideMenuOnStart() {
-    menu.style.display = "none";
-    hoverMenu.style.display = "none";
-}
