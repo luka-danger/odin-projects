61e7f0e72b0fb70ad4cd08e245c2cb834902c973
luka-danger <lukathevegan@gmail.com>
2025-03-08 17:02:01 -0700

    Initial commit
    Created constructors

---
+++ dsa/linked-list/linked-list.js
@@ -0,0 +1,15 @@
+// Node constructor
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+// Linked List constructor of size 1
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+}
