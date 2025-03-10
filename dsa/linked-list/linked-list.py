b635b91bdadbfb5b4f1a7d7e2a5215b77a5a7ae9
luka-danger <lukathevegan@gmail.com>
2025-03-10 07:56:58 -0600

    Create Python file

---
+++ dsa/linked-list/linked-list.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# LL Constructor w/ one node
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    #def append(self, value):
+    #def prepend(self, value):
+    #def insert(self, value):
+
+my_linked_list = LinkedList(7)
+
+print(my_linked_list.head.value)
