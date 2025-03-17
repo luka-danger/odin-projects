7a41d023c1d9102447a5fa0e74865fdda898e98e
luka-danger <lukathevegan@gmail.com>
2025-03-16 23:10:12 -0600

    Finish test cases

--- dsa/linked-list/JavaScript/test-cases.js
+++ dsa/linked-list/JavaScript/test-cases.js
@@ -13,9 +13,11 @@ coolList.append(6)
 // Test Prepend Method
 coolList.prepend(0)
 
+
 // Test contains method
 console.log(coolList.contains(19))
 console.log(coolList.contains(3))
+
 
 // Test insert method
 coolList.insert(3, 'dance party!!!!')
@@ -27,16 +29,16 @@ coolList.prepend('remove me')
 coolList.remove(0)
 coolList.printList()
 
+
 // Test Get Value at Index Method
 console.log(`Index 0 value: ${coolList.getValueAtIndex(0).value} `)
 console.log(`Index 3 value: ${coolList.getValueAtIndex(3).value} `)
+
 
 // Test To String Method
 coolList.remove(7)
 coolList.remove(3)
 coolList.pop()
 console.log(coolList.toString())
-
-
 
 
