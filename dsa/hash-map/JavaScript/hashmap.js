e2b5b15706947fe1dcd5d9e2784c1d5986840629
luka-danger <lukathevegan@gmail.com>
2025-04-06 12:09:23 -0600

    Refactor!
    Add linkedlist and open addressing to optimize performance.
    The hashmap uses linkedlist nodes (chaining) to prevent collisions, and uses load factor / capacity to prevent the number of collisions while also preventing the hashmap from growing too big, too quickly. This optimizes retrieval time and reduces space taken in memory.

--- dsa/hash-map/JavaScript/hashmap.js
+++ dsa/hash-map/JavaScript/hashmap.js
@@ -1,8 +1,31 @@
 import { Node } from "./new-node.js";
 
 class HashMap{
-    constructor() {
-        this.buckets = new Array(16);
+    constructor(loadFactor = 0.8, capacity = 16) {
+        this.buckets = new Array(capacity);
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.size = 0;
+    }
+
+    increaseCapacity() {
+        this.capacity *= 2;
+        // Create new buckets after resizing array
+        const newBuckets = new Array(this.capacity)
+
+        // Rehash the resized hashmap
+        for (let i = 0; i < this.buckets.length; i++) {
+            let current = this.buckets[i]
+            while (current) {
+                // Re-index entry 
+                const index = this.hash(current.key) % this.capacity
+                let newNode = new Node(current.key, current.value) 
+                newNode.next = newBuckets[index]
+                current = current.next
+            }
+        }
+        // Assign newly hashed buckets to buckets in resized hashmap
+        this.buckets = newBuckets;
     }
 
     hash(key) {
@@ -16,52 +39,99 @@ class HashMap{
     }
 
     printAll() {
-        // 🚨 FIX ME: Create an if that lets user know if nothing is in the hashtable
-
+        if (this.size == 0) {
+            console.log('No key-value pairs in hashtable!')
+        }
         for (let i = 0; i < this.buckets.length; i++) {
-            if (this.buckets[i]) {
-                this.buckets[i].forEach(([key, value]) => console.log(`${key}: ${value}`))
+            let current = this.buckets[i]
+            while (current) {
+                console.log(`${current.key}: ${current.value}`)
+                current = current.next;
             }
         }
     }
 
     insert(key, value) {
-        let index = this.hash(key)
-        if (!this.buckets[index]) {
-            this.buckets[index] = []
+        const index = this.hash(key) % this.capacity;
+
+        // Prevent dupliate keys
+        let current = this.buckets[index]
+        while (current) {
+            if (current.key === key) {
+                current.value = value
+                return
+            }
+            current = current.next;
         }
-        this.buckets[index].push([key, value])
+        
+        const newNode = new Node(key, value);
+        // If bucket is empty
+        if (!this.buckets[index]) {
+            this.buckets[index] = newNode;
+        }
+        else {
+            current = this.buckets[index]
+            while (current.next) {
+                current = current.next
+            }
+            current.next = newNode
+        }
+        this.size ++;
+        
+        if (this.size >= this.capacity * this.loadFactor) {
+            this.increaseCapacity();
+        }
     }
 
     getValue(key) {
-        let index = this.hash(key)
-        if (this.buckets[index]) {
-            for (let i = 0; i < this.buckets[index].length; i++) {
-                if (this.buckets[index][i][0] == key) {
-                    return `Value: ${this.buckets[index][i][1]}, Key: ${key}`
-                }
+        const index = this.hash(key) % this.capacity;
+        
+        let current = this.buckets[index];
+        while (current) {
+            if (current.key === key) {
+                return current.value
             }
+            current = current.next
         }
+        return 'Key not in hashmap!'    
     }
 
     hasKey(key) {
-        let index = this.hash(key)
-        if (this.buckets[index]) {
-            for (let i = 0; i < this.buckets[index].length; i++) {
-                return 'True' ? this.buckets[index][i][0] == key : 'False'
+        const index = this.hash(key) % this.capacity;
+
+        let current = this.buckets[index]
+        while(current) {
+            if (current.key === key) {
+                return true;
             }
+            current = current.next
         }
+        return false
     }
 
     removeKey(key) {
-        let index = this.hash(key)
-        if (this.buckets[index]) {
-            for (let i = 0; i < this.buckets[index].length; i++) {
-                if (this.buckets[index][i][0] == key) {
-                    this.buckets[index].splice(i, 1)
+        const index = this.hash(key) % this.capacity;
+        let current = this.buckets[index];
+        let prev = null;
+
+        while(current) {
+            if (current.key === key) {
+                if (prev) {
+                    prev.next = current.next
                 }
+                else {
+                    this.buckets[index] = current.next
+                }
+                this.size --;
+                return 
             }
+            prev = current;
+            current = current.next;
         }
+    }
+
+    getSize() {
+        return this.size;
     }
 }
 
@@ -70,19 +140,28 @@ let myMap = new HashMap()
 myMap.insert(1, 'noodles')
 myMap.insert(2, 'grapes')
 myMap.insert(3, 'kiwis')
+myMap.insert(5, 'mangos')
 
 myMap.printAll()
 
-// console.log(myMap.getValue(2))
+console.log(myMap.getValue(2))
+console.log(myMap.getValue(5))
+console.log(myMap.getValue(1))
+console.log(myMap.getValue(4))
 
-// console.log(myMap.hasKey(1))
+console.log(myMap.hasKey(1))
+console.log(myMap.hasKey(55))
 
-// console.log(myMap.hasKey(55))
-
-myMap.removeKey(1)
+/*
 myMap.removeKey(2)
+myMap.removeKey(1)
 myMap.removeKey(3)
+*/
+
+myMap.removeKey(5)
 
 myMap.printAll()
+
+console.log(`Size: ${myMap.getSize()}`)
 
 
