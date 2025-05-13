1a243b03d6b8ed682bcd7b8cbe324917c1f5ae8d
luka-danger <lukathevegan@gmail.com>
2025-05-13 10:36:52 -0600

    Create methods
    Recursive delete, delete, and 2 helper functions: minValue and find

--- dsa/trees/JavaScript/bst.js
+++ dsa/trees/JavaScript/bst.js
@@ -61,6 +61,67 @@ export class Tree {
             }
         }
     }
+
+    __delete(current, value) {
+        // If tree is empty
+        if (!this.root) return console.log('Tree empty')
+        // If value not in tree
+        if (current === null) {
+            console.log(`${value} not in binary tree`)
+            return null;
+        }
+        // If value is larger than current node value -> recursively move right 
+        if (value > current.value) {
+            current.right = this.__delete(current.right, value)
+        }
+        // If value is less than current node value -> recursively move left
+        else if (value < current.value) {
+            current.left = this.__delete(current.left, value)
+        }
+        else {
+            // If node has one or no child 
+            if (current.left === null) return current.right;
+            if (current.right === null) return current.left;
+
+            // If subtree node has two children
+            // Find min value of subtree
+            let subTreeMin = this.minValue(current.right.value)
+            // Set value of current node to min value of subtree
+            current.value = subTreeMin
+            // Remove subtree min value
+            current.right = this.__delete(current.right, subTreeMin)
+            
+        }
+        return current
+    }
+
+    minValue(value) {
+        // Use find function to set value 
+        let current = this.find(value)
+        // If value not in binary tree
+        if (!current) return `${value} not in binary tree`
+        // While a left node exists in subtree, continue moving left
+        while (current.left) {
+            current = current.left
+        }
+        // Return lowest value of subtree
+        return current.value; 
+    }
+
+    find(value, current = this.root) {
+        // If value does not exist in tree
+        if (!current) return null;
+        // Return value when found
+        if (current.value === value) return current;
+        // If value is less than current node, recursively move left down tree
+        if (value < current.value) return this.find(value, current.left);
+        // Else, value must be larger than current node, recursively move right down tree
+        return this.find(value, current.right);
+    }
+
+    delete(value) {
+        this.root = this.__delete(this.root, value)
+    }
 }
 
 let myTree = new Tree()
@@ -69,11 +130,19 @@ let myArr = [1, 2, 3, 4, 5, 6, 7]
 
 myTree.buildTree(myArr)
 
+
 console.log(myTree.root.value)
 console.log(myTree.root.left.value)
 console.log(myTree.root.right.value)
 
-console.log(myTree.insert(4))
-console.log(myTree.insert(12))
+myTree.insert(12)
+myTree.insert(9)
+myTree.insert(15)
 
+myTree.delete(9)
+myTree.delete(12)
+myTree.delete(12)
 myTree.prettyPrint(myTree.root)
+
+console.log(myTree.minValue(12))
+
